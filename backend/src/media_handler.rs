a9526983fd1a1c17b539a14e9763bd3283967992
microwonk <nicolas.theo.frey@gmail.com>
2024-10-04 01:27:03 +0200

    image + video compression

--- backend/src/media_handler.rs
+++ backend/src/media_handler.rs
@@ -1,3 +1,5 @@
+use std::{io::BufWriter, num::NonZeroU32, process::Command};
+
 use crate::{admin_check, ok, ApiError, ApiResult, Media, MediaNoData, ServerState, User};
 use axum::{
     body::Bytes,
@@ -6,6 +8,13 @@ use axum::{
     response::IntoResponse,
     Extension, Json,
 };
+
+use fast_image_resize as fr;
+use image::{
+    codecs::{jpeg::JpegEncoder, png::PngEncoder},
+    ColorType, ImageEncoder, ImageReader,
+};
+use tokio::{fs::File, io::AsyncWriteExt};
 use uuid::Uuid;
 
 pub async fn upload(
@@ -59,6 +68,37 @@ pub async fn upload(
                 }
             };
 
+            let compressed_data = match content_type.as_str() {
+                "image/jpeg" | "image/png" => match compress_image(&data, content_type.as_str()) {
+                    Ok(compressed_img) => compressed_img,
+                    Err(err) => {
+                        failures.push(format!(
+                            "Image compression failed for file: {} with error: {}",
+                            file_name, err
+                        ));
+                        continue;
+                    }
+                },
+                "image/gif" => {
+                    // TODO: implement gif compression
+                    data
+                }
+                "video/mp4" => match compress_video_mp4(&data).await {
+                    Ok(compressed_video) => compressed_video,
+                    Err(err) => {
+                        failures.push(format!(
+                            "Video compression failed for file: {} with error: {}",
+                            file_name, err
+                        ));
+                        continue;
+                    }
+                },
+                _ => {
+                    failures.push(format!("Unsupported content type for file: {}", file_name));
+                    continue;
+                }
+            };
+
             // Try to insert media into the database
             match sqlx::query_as::<_, Media>(
                 r#"
@@ -69,7 +109,7 @@ pub async fn upload(
             )
             .bind(post_id)
             .bind(file_name.clone())
-            .bind(data) // Store the binary data
+            .bind(compressed_data) // Store the compressed data
             .bind(content_type) // directly store MIME
             .fetch_one(&state.pool)
             .await
@@ -88,6 +128,124 @@ pub async fn upload(
 
     // Return the response
     ok!(response)
+}
+
+fn compress_image(data: &[u8], content_type: &str) -> Result<Vec<u8>, String> {
+    // Convert DynamicImage to an RGBA buffer
+    let img = ImageReader::new(std::io::Cursor::new(data))
+        .with_guessed_format()
+        .unwrap()
+        .decode()
+        .unwrap();
+    let width = NonZeroU32::new(img.width()).unwrap();
+    let height = NonZeroU32::new(img.height()).unwrap();
+    let mut src_image = fr::Image::from_vec_u8(
+        width,
+        height,
+        img.to_rgba8().into_raw(),
+        fr::PixelType::U8x4,
+    )
+    .unwrap();
+
+    // Multiple RGB channels of source image by alpha channel
+    // (not required for the Nearest algorithm)
+    let alpha_mul_div = fr::MulDiv::default();
+    alpha_mul_div
+        .multiply_alpha_inplace(&mut src_image.view_mut())
+        .unwrap();
+
+    // Create container for data of destination image
+    let dst_width = NonZeroU32::new(480).unwrap();
+    let dst_height = NonZeroU32::new(360).unwrap();
+    let mut dst_image = fr::Image::new(dst_width, dst_height, src_image.pixel_type());
+
+    // Get mutable view of destination image data
+    let mut dst_view = dst_image.view_mut();
+
+    // Create Resizer instance and resize source image
+    // into buffer of destination image
+    let mut resizer = fr::Resizer::new(fr::ResizeAlg::Convolution(fr::FilterType::Lanczos3));
+    resizer.resize(&src_image.view(), &mut dst_view).unwrap();
+
+    // Divide RGB channels of destination image by alpha
+    alpha_mul_div.divide_alpha_inplace(&mut dst_view).unwrap();
+
+    // Write destination image as PNG/JPEG-file
+    let mut result_buf = BufWriter::new(Vec::new());
+
+    match content_type {
+        "image/jpeg" => JpegEncoder::new(&mut result_buf)
+            .write_image(
+                dst_image.buffer(),
+                dst_width.get(),
+                dst_height.get(),
+                ColorType::Rgba8.into(),
+            )
+            .unwrap(),
+        "image/png" => PngEncoder::new(&mut result_buf)
+            .write_image(
+                dst_image.buffer(),
+                dst_width.get(),
+                dst_height.get(),
+                ColorType::Rgba8.into(),
+            )
+            .unwrap(),
+        _ => return Err("Unsupported image format".to_string()),
+    }
+
+    let image_bytes = result_buf.into_inner().unwrap();
+
+    Ok(image_bytes)
+}
+
+// Function to compress MP4 video using `ffmpeg`
+async fn compress_video_mp4(data: &[u8]) -> Result<Vec<u8>, String> {
+    // Create temporary input and output files for processing
+    let input_path = "public/input_video.mp4";
+    let output_path = "public/output_video_compressed.mp4";
+
+    // Write the video data to a temporary input file
+    let mut input_file = File::create(input_path).await.map_err(|e| e.to_string())?;
+    input_file
+        .write_all(data)
+        .await
+        .map_err(|e| e.to_string())?;
+    input_file.sync_all().await.map_err(|e| e.to_string())?;
+
+    // Run the `ffmpeg` command to compress the video
+    let status = Command::new("ffmpeg")
+        .args([
+            "-i",
+            input_path, // Input file
+            "-vcodec",
+            "libx264", // Use H.264 video codec for compression
+            "-crf",
+            "28", // Constant Rate Factor (CRF) for quality (28 is a reasonable default)
+            "-preset",
+            "fast",      // Preset for speed/quality tradeoff
+            output_path, // Output file
+        ])
+        .status()
+        .map_err(|e| format!("Failed to run ffmpeg: {}", e))?;
+
+    if !status.success() {
+        return Err("ffmpeg failed to compress the video".to_string());
+    }
+
+    // Read the compressed video from the output file
+    let compressed_data = tokio::fs::read(output_path)
+        .await
+        .map_err(|e| format!("Failed to read compressed video: {}", e))?;
+
+    // Clean up the temporary files
+    tokio::fs::remove_file(input_path)
+        .await
+        .map_err(|e| format!("Failed to remove input video: {}", e))?;
+    tokio::fs::remove_file(output_path)
+        .await
+        .map_err(|e| format!("Failed to remove compressed video: {}", e))?;
+
+    Ok(compressed_data)
 }
 
 pub async fn get_upload(
